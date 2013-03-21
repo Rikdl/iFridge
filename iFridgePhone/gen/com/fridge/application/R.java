ee5c3facba70b2a94e80e65a0c2987c9c66fba86
Marijn <marijn.bel@hotmailcom>
2013-03-21 14:35:19 +0100

    commit nothing special

--- iFridgePhone/gen/com/fridge/application/R.java
+++ iFridgePhone/gen/com/fridge/application/R.java
@@ -29,19 +29,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int date_listview=0x7f060007;
+        public static final int date_listview=0x7f060008;
         public static final int imageView_display=0x7f060000;
-        public static final int listview_search=0x7f060004;
-        public static final int product_listview=0x7f060006;
-        public static final int search_search=0x7f060002;
-        public static final int searchfield_search=0x7f060003;
+        public static final int listview_search=0x7f060005;
+        public static final int product_listview=0x7f060007;
+        public static final int search_search=0x7f060003;
+        public static final int searchfield_search=0x7f060004;
+        public static final int startBtn=0x7f060002;
         public static final int textView_display=0x7f060001;
-        public static final int tlSearchRecipe=0x7f060005;
+        public static final int tlSearchRecipe=0x7f060006;
     }
     public static final class layout {
         public static final int display=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int product_fridge_listview=0x7f030002;
+        public static final int dowload=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int product_fridge_listview=0x7f030003;
     }
     public static final class string {
         public static final int ExpireDate=0x7f050003;
