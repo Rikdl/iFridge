f9190d0cb2414b4fa293e2980cdd93b205377a48
Yannick Geerts <yannick.g@skynet.be>
2013-04-19 14:37:17 +0200

    bsbb

---
+++ Simple-Android-OCR-Own/src/com/datumdroid/android/ocr/simple/CameraActivity.java
@@ -0,0 +1,24 @@
+package com.datumdroid.android.ocr.simple;
+
+import android.hardware.Camera;
+
+public class CameraActivity {
+	
+	
+	
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
+	}
+	
+	
+
+}
+
